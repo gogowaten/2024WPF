2a9c6689efe85ecae1c638372c876dc6ed707d9c
waten <gogowaten@yahoo.co.jp>
2024-11-24 14:36:22 +0900

    202411241436

---
+++ 20241124/CustomControl1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20241124
+{
+    /// <summary>
+    /// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    ///
+    /// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_20241124"
+    ///
+    ///
+    /// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_20241124;assembly=_20241124"
+    ///
+    /// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    /// リビルドして、コンパイル エラーを防ぐ必要があります:
+    ///
+    ///     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    ///     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを参照し、選択します。
+    ///
+    ///
+    /// 手順 2)
+    /// コントロールを XAML ファイルで使用します。
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class CustomControl1 : Thumb
+    {
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(CustomControl1),
+                new FrameworkPropertyMetadata(string.Empty,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+
+            DataContext = this;
+        }
+
+        //public override void OnApplyTemplate()
+        //{
+        //    base.OnApplyTemplate();
+        //    Thumb tt = this.GetTemplateChild("PART_Thumb") as Thumb;
+        //}
+    }
+
+    //public class CustomControl2 : Thumb
+    //{
+
+    //    public Brush Fill
+    //    {
+    //        get { return (Brush)GetValue(FillProperty); }
+    //        set { SetValue(FillProperty, value); }
+    //    }
+    //    public static readonly DependencyProperty FillProperty =
+    //        DependencyProperty.Register(nameof(Fill), typeof(Brush), typeof(CustomControl2),
+    //            new FrameworkPropertyMetadata(Brushes.Beige,
+    //                FrameworkPropertyMetadataOptions.AffectsRender |
+    //                FrameworkPropertyMetadataOptions.AffectsMeasure |
+    //                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    //    public CustomControl2()
+    //    {
+    //        DefaultStyleKeyProperty.OverrideMetadata(
+    //            typeof(CustomControl2), new FrameworkPropertyMetadata(typeof(CustomControl2)));
+
+    //        DataContext = this;
+
+    //    }
+    //}
+
+
+
+
+
+}
---
+++ 20241124/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace _20241124
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddEllipseThumb();
+        }
+
+        private void AddEllipseThumb()
+        {
+            //MyCanvas.Children.Add(new CustomControl2() { Width = 50, Height = 50, Fill = Brushes.Red, Background = Brushes.MediumPurple });
+            //var aa = new CustomControl2();
+            
+
+        }
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is UIElement element)
+            {
+                Canvas.SetLeft(element, Canvas.GetLeft(element) + e.HorizontalChange);
+                Canvas.SetTop(element, Canvas.GetTop(element) + e.VerticalChange);
+            }
+        }
+
+
+    }
+}
