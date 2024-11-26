10a42422b0fa9c8e1c48618882c4a709bbd03b97
waten <gogowaten@yahoo.co.jp>
2024-11-26 12:57:09 +0900

    202411261257

---
+++ 20241126/ItemsThumb.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20241126
+{
+    /// <summary>
+    /// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    ///
+    /// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_20241126"
+    ///
+    ///
+    /// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_20241126;assembly=_20241126"
+    ///
+    /// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    /// リビルドして、コンパイル エラーを防ぐ必要があります:
+    ///
+    ///     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    ///     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを参照し、選択します。
+    ///
+    ///
+    /// 手順 2)
+    /// コントロールを XAML ファイルで使用します。
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+
+
+    [ContentProperty(nameof(MyItems))]
+    public class ItemsThumb : Thumb
+    {
+        static ItemsThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ItemsThumb), new FrameworkPropertyMetadata(typeof(ItemsThumb)));
+        }
+
+
+        #region DependencyProperty
+
+
+        //public ObservableCollection<UIElement> MyItems
+        //{
+        //    get { return (ObservableCollection<UIElement>)GetValue(MyItemsProperty); }
+        //    set { SetValue(MyItemsProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyItemsProperty =
+        //    DependencyProperty.Register(nameof(MyItems),
+        //        typeof(ObservableCollection<UIElement>), typeof(ItemsThumb),
+        //        new FrameworkPropertyMetadata(new ObservableCollection<UIElement>(),
+        //            FrameworkPropertyMetadataOptions.AffectsRender |
+        //            FrameworkPropertyMetadataOptions.AffectsMeasure |
+        //            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        //↑は無限ループになる
+
+        //FrameworkPropertyMetadata(null,
+        //ここはnull、もしここで初期化するとネストした時に無限ループになるので
+        //初期化はコンストラクタ内で行うことにした
+        public ObservableCollection<UIElement> MyItems
+        {
+            get { return (ObservableCollection<UIElement>)GetValue(MyItemsProperty); }
+            set { SetValue(MyItemsProperty, value); }
+        }
+        public static readonly DependencyProperty MyItemsProperty =
+            DependencyProperty.Register(nameof(MyItems),
+                typeof(ObservableCollection<UIElement>), typeof(ItemsThumb),
+                new FrameworkPropertyMetadata(null,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double MyX
+        {
+            get { return (double)GetValue(MyXProperty); }
+            set { SetValue(MyXProperty, value); }
+        }
+        public static readonly DependencyProperty MyXProperty =
+            DependencyProperty.Register(nameof(MyX), typeof(double), typeof(ItemsThumb),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double MyY
+        {
+            get { return (double)GetValue(MyYProperty); }
+            set { SetValue(MyYProperty, value); }
+        }
+        public static readonly DependencyProperty MyYProperty =
+            DependencyProperty.Register(nameof(MyY), typeof(double), typeof(ItemsThumb),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+
+
+        #endregion DependencyProperty
+
+        public ItemsThumb()
+        {
+            MyItems = [];
+            DataContext = this;
+        }
+
+    }
+
+    public class TextBlockThumb : Thumb
+    {
+        static TextBlockThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBlockThumb), new FrameworkPropertyMetadata(typeof(TextBlockThumb)));
+        }
+
+        #region 依存関係プロパティ
+        public string MyText
+        {
+            get { return (string)GetValue(MyTextProperty); }
+            set { SetValue(MyTextProperty, value); }
+        }
+        public static readonly DependencyProperty MyTextProperty =
+            DependencyProperty.Register(nameof(MyText), typeof(string), typeof(TextBlockThumb),
+                new FrameworkPropertyMetadata(string.Empty,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double MyX
+        {
+            get { return (double)GetValue(MyXProperty); }
+            set { SetValue(MyXProperty, value); }
+        }
+        public static readonly DependencyProperty MyXProperty =
+            DependencyProperty.Register(nameof(MyX), typeof(double), typeof(TextBlockThumb),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double MyY
+        {
+            get { return (double)GetValue(MyYProperty); }
+            set { SetValue(MyYProperty, value); }
+        }
+        public static readonly DependencyProperty MyYProperty =
+            DependencyProperty.Register(nameof(MyY), typeof(double), typeof(TextBlockThumb),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        #endregion 依存関係プロパティ
+        public TextBlockThumb() { DataContext = this; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 20241126/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace _20241126
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ItemsThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is UIElement ue)
+            {
+                Canvas.SetLeft(ue, Canvas.GetLeft(ue) + e.HorizontalChange);
+                Canvas.SetTop(ue, Canvas.GetTop(ue) + e.VerticalChange);
+            }
+        }
+    }
+}
