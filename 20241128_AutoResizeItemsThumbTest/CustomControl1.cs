ef10cdb4b635fac4644cd6fe27432af93bbb55d0
waten <gogowaten@yahoo.co.jp>
2024-11-28 06:54:13 +0900

    202411280654

---
+++ 20241128_AutoResizeItemsThumbTest/CustomControl1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20241128_AutoResizeItemsThumbTest
+{
+    /// <summary>
+    /// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    ///
+    /// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_20241128_AutoResizeItemsThumbTest"
+    ///
+    ///
+    /// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_20241128_AutoResizeItemsThumbTest;assembly=_20241128_AutoResizeItemsThumbTest"
+    ///
+    /// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    /// リビルドして、コンパイル エラーを防ぐ必要があります:
+    ///
+    ///     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    ///     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを参照し、選択します。
+    ///
+    ///
+    /// 手順 2)
+    /// コントロールを XAML ファイルで使用します。
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public abstract class CustomBase : Thumb
+    {
+        static CustomBase()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomBase), new FrameworkPropertyMetadata(typeof(CustomBase)));
+        }
+        #region 依存関係プロパティ
+
+        public double MyX
+        {
+            get { return (double)GetValue(MyXProperty); }
+            set { SetValue(MyXProperty, value); }
+        }
+        public static readonly DependencyProperty MyXProperty =
+            DependencyProperty.Register(nameof(MyX), typeof(double), typeof(CustomBase),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double MyY
+        {
+            get { return (double)GetValue(MyYProperty); }
+            set { SetValue(MyYProperty, value); }
+        }
+        public static readonly DependencyProperty MyYProperty =
+            DependencyProperty.Register(nameof(MyY), typeof(double), typeof(CustomBase),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        #endregion 依存関係プロパティ
+        public CustomBase()
+        {
+            SetBinding(AutoResizeCanvas.LeftProperty, new Binding()
+            {
+                Source = this,
+                Path = new PropertyPath(MyXProperty)
+            });
+            SetBinding(AutoResizeCanvas.TopProperty, new Binding()
+            {
+                Source = this,
+                Path = new PropertyPath(MyYProperty)
+            });
+
+        }
+    }
+
+    [ContentProperty(nameof(MyItems))]
+    public class CustomItemsThumb : CustomBase
+    {
+        static CustomItemsThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomItemsThumb), new FrameworkPropertyMetadata(typeof(CustomItemsThumb)));
+        }
+        public CustomItemsThumb()
+        {
+            DataContext = this;
+            MyItems = [];
+        }
+
+
+        public ObservableCollection<UIElement> MyItems
+        {
+            get { return (ObservableCollection<UIElement>)GetValue(MyItemsProperty); }
+            set { SetValue(MyItemsProperty, value); }
+        }
+        public static readonly DependencyProperty MyItemsProperty =
+            DependencyProperty.Register(nameof(MyItems), typeof(ObservableCollection<UIElement>), typeof(CustomItemsThumb),
+                new FrameworkPropertyMetadata(null,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    }
+
+
+    public class CustomTextThumb : CustomBase
+    {
+        static CustomTextThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomTextThumb), new FrameworkPropertyMetadata(typeof(CustomTextThumb)));
+        }
+
+        public string MyText
+        {
+            get { return (string)GetValue(MyTextProperty); }
+            set { SetValue(MyTextProperty, value); }
+        }
+        public static readonly DependencyProperty MyTextProperty =
+            DependencyProperty.Register(nameof(MyText), typeof(string), typeof(CustomTextThumb),
+                new FrameworkPropertyMetadata(string.Empty,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public CustomTextThumb()
+        {
+            DataContext = this;
+        }
+    }
+
+}
---
+++ 20241128_AutoResizeItemsThumbTest/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+//AutoResizeCanvasを使ったThumbのテスト
+//スクロールバー表示しているときに外側のThumbを移動すると一気に縮小する動作がいまいち
+//できればドラッグ移動終了時にリサイズできるようにしたい
+namespace _20241128_AutoResizeItemsThumbTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddThumbs(200);
+        }
+
+        private void AddThumbs(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                CustomTextThumb thumb = new() { MyText = "text", MyX = i * 10, MyY = i * 10 };
+                thumb.DragDelta += CustomTextThumb_DragDelta;
+                MyItemsThumb.MyItems.Add(thumb);
+            }
+        }
+
+        private void CustomTextThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is CustomBase cb)
+            {
+                cb.MyX += e.HorizontalChange;
+                cb.MyY += e.VerticalChange;
+            }
+        }
+    }
+}
