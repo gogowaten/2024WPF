4bd13954cf83680dcbfb2ca492188ca5203bd150
waten <gogowaten@yahoo.co.jp>
2024-12-04 20:47:18 +0900

    202412042047

---
+++ 20241204_FocusTabNaviMode/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace _20241204_FocusTabNaviMode
+{
+    public partial class MainWindow : Window
+    {
+        public Array MyEnumArray { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            MyEnumArray = Enum.GetValues(typeof(KeyboardNavigationMode));
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                tb.SelectAll();
+            }
+        }
+
+    }
+}
