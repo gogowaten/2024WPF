1a98f27f07841233db6139e4ef6c164aafa57dd8
waten <gogowaten@yahoo.co.jp>
2024-12-08 14:53:22 +0900

    202412081453

---
+++ 20241207_ItemsControlCanvasPanelThumb/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace _20241207_ItemsControlCanvasPanelThumb
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is Thumb t)
+            {
+                Canvas.SetLeft(t, Canvas.GetLeft(t) + e.HorizontalChange);
+                Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
+                e.Handled = true;
+                //↑ trueにしないと、重なっているThumb全てにイベントが伝播して動いてしまう
+            }
+        }
+    }
+}
