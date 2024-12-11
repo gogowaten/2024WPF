835977375e67a4b5d5fc0dfa77354b29d2f4a4f4
waten <gogowaten@yahoo.co.jp>
2024-12-11 15:27:11 +0900

    202412111527

---
+++ 20241211_Style_Inheritance/CustomControl1.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace _20241211_Style_Inheritance
+{
+
+
+    public abstract class KisoThumb : Thumb
+    {
+        #region 依存関係プロパティ
+
+        public double MyLeft
+        {
+            get { return (double)GetValue(MyLeftProperty); }
+            set { SetValue(MyLeftProperty, value); }
+        }
+        public static readonly DependencyProperty MyLeftProperty =
+            DependencyProperty.Register(nameof(MyLeft), typeof(double), typeof(KisoThumb), new PropertyMetadata(0.0));
+
+        public double MyTop
+        {
+            get { return (double)GetValue(MyTopProperty); }
+            set { SetValue(MyTopProperty, value); }
+        }
+        public static readonly DependencyProperty MyTopProperty =
+            DependencyProperty.Register(nameof(MyTop), typeof(double), typeof(KisoThumb), new PropertyMetadata(0.0));
+
+        public string MyText
+        {
+            get { return (string)GetValue(MyTextProperty); }
+            set { SetValue(MyTextProperty, value); }
+        }
+        public static readonly DependencyProperty MyTextProperty =
+            DependencyProperty.Register(nameof(MyText), typeof(string), typeof(KisoThumb), new PropertyMetadata(string.Empty));
+
+        #endregion 依存関係プロパティ
+        static KisoThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(KisoThumb), new FrameworkPropertyMetadata(typeof(KisoThumb)));
+        }
+        public KisoThumb()
+        {
+            DataContext = this;
+        }
+    }
+
+    public class TextBlockThumb : KisoThumb
+    {
+        #region 依存関係プロパティ
+
+        
+        #endregion 依存関係プロパティ
+        static TextBlockThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBlockThumb), new FrameworkPropertyMetadata(typeof(TextBlockThumb)));
+        }
+
+    }
+
+    public class EllipseTextThumb : TextBlockThumb
+    {
+        #region 依存関係プロパティ
+
+        public double MySize
+        {
+            get { return (double)GetValue(MySizeProperty); }
+            set { SetValue(MySizeProperty, value); }
+        }
+        public static readonly DependencyProperty MySizeProperty =
+            DependencyProperty.Register(nameof(MySize), typeof(double), typeof(EllipseTextThumb), new PropertyMetadata(30.0));
+        #endregion 依存関係プロパティ
+
+        static EllipseTextThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EllipseTextThumb), new FrameworkPropertyMetadata(typeof(EllipseTextThumb)));
+        }
+
+    }
+
+}
---
+++ 20241211_Style_Inheritance/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace _20241211_Style_Inheritance
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is KisoThumb t)
+            {
+                t.MyLeft += e.HorizontalChange;
+                t.MyTop += e.VerticalChange;
+            }
+        }
+    }
+}
