bcf5bb53f2f210a7154e281c7957e828abe161fa
waten <gogowaten@yahoo.co.jp>
2024-12-14 22:31:54 +0900

    202412142231

--- 20241211/CustomControl1.cs
+++ 20241211/CustomControl1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,48 @@ using System.Windows.Shapes;
 
 namespace _20241211
 {
+
+
+    /// <summary>
+    /// 基礎Thumb、すべてのCustomControlThumbの派生元
+    /// </summary>
+    [DebuggerDisplay("text = {MyText}, left = {MyLeft}, top = {MyTop}, AcW = {ActualWidth}, AcH = {ActualHeight}")]
     public abstract class KisoThumb : Thumb
     {
         #region 依存関係プロパティ
+
+
+        //public double MyLeft
+        //{
+        //    get { return (double)GetValue(MyLeftProperty); }
+        //    set { SetValue(MyLeftProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyLeftProperty =
+        //    DependencyProperty.Register(nameof(MyLeft), typeof(double), typeof(KisoThumb),
+        //        new FrameworkPropertyMetadata(0.0,
+        //            FrameworkPropertyMetadataOptions.AffectsRender |
+        //            FrameworkPropertyMetadataOptions.AffectsMeasure |
+        //            FrameworkPropertyMetadataOptions.AffectsArrange |
+        //            FrameworkPropertyMetadataOptions.Inherits |
+        //            FrameworkPropertyMetadataOptions.AffectsParentArrange |
+        //            FrameworkPropertyMetadataOptions.AffectsParentMeasure |
+        //            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        //public double MyTop
+        //{
+        //    get { return (double)GetValue(MyTopProperty); }
+        //    set { SetValue(MyTopProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyTopProperty =
+        //    DependencyProperty.Register(nameof(MyTop), typeof(double), typeof(KisoThumb),
+        //        new FrameworkPropertyMetadata(0.0,
+        //            FrameworkPropertyMetadataOptions.AffectsRender |
+        //            FrameworkPropertyMetadataOptions.AffectsMeasure |
+        //            FrameworkPropertyMetadataOptions.AffectsArrange |
+        //            FrameworkPropertyMetadataOptions.Inherits |
+        //            FrameworkPropertyMetadataOptions.AffectsParentArrange |
+        //            FrameworkPropertyMetadataOptions.AffectsParentMeasure |
+        //            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
         public double MyLeft
         {
@@ -39,9 +79,19 @@ namespace _20241211
         public static readonly DependencyProperty MyTopProperty =
             DependencyProperty.Register(nameof(MyTop), typeof(double), typeof(KisoThumb), new PropertyMetadata(0.0));
 
-        
+        public string MyText
+        {
+            get { return (string)GetValue(MyTextProperty); }
+            set { SetValue(MyTextProperty, value); }
+        }
+        public static readonly DependencyProperty MyTextProperty =
+            DependencyProperty.Register(nameof(MyText), typeof(string), typeof(KisoThumb), new PropertyMetadata(string.Empty));
+
         #endregion 依存関係プロパティ
 
+        //親要素の識別用。自身がグループ化されたときに親要素のGroupThumbを入れておく
+        public GroupThumb? MyParentThumb { get; internal set; }
+        //public GroupThumb2? MyParentThumb2 { get; internal set; }
         static KisoThumb()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(KisoThumb), new FrameworkPropertyMetadata(typeof(KisoThumb)));
@@ -49,58 +99,56 @@ namespace _20241211
         public KisoThumb()
         {
             DataContext = this;
+            Focusable = true;
         }
     }
 
     public class TextBlockThumb : KisoThumb
     {
-        #region 依存関係プロパティ
-
-        public string MyText
-        {
-            get { return (string)GetValue(MyTextProperty); }
-            set { SetValue(MyTextProperty, value); }
-        }
-        public static readonly DependencyProperty MyTextProperty =
-            DependencyProperty.Register(nameof(MyText), typeof(string), typeof(TextBlockThumb), new PropertyMetadata(string.Empty));
-
-        #endregion 依存関係プロパティ
         static TextBlockThumb()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBlockThumb), new FrameworkPropertyMetadata(typeof(TextBlockThumb)));
         }
-        public TextBlockThumb()
-        {
-            
-        }
+
     }
 
+    public class EllipseTextThumb : TextBlockThumb
+    {
+        #region 依存関係プロパティ
+
+        public double MySize
+        {
+            get { return (double)GetValue(MySizeProperty); }
+            set { SetValue(MySizeProperty, value); }
+        }
+        public static readonly DependencyProperty MySizeProperty =
+            DependencyProperty.Register(nameof(MySize), typeof(double), typeof(EllipseTextThumb), new PropertyMetadata(30.0));
+        #endregion 依存関係プロパティ
+
+        static EllipseTextThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EllipseTextThumb), new FrameworkPropertyMetadata(typeof(EllipseTextThumb)));
+        }
+
+    }
 
     [ContentProperty(nameof(MyThumbs))]
     public class GroupThumb : KisoThumb
     {
+        #region 依存関係プロパティ
 
-        //public ObservableCollection<Thumb> MyThumbs
-        //{
-        //    get { return (ObservableCollection<Thumb>)GetValue(MyThumbsProperty); }
-        //    set { SetValue(MyThumbsProperty, value); }
-        //}
-        //public static readonly DependencyProperty MyThumbsProperty =
-        //    DependencyProperty.Register(nameof(MyThumbs), typeof(ObservableCollection<Thumb>), typeof(GroupThumb), new FrameworkPropertyMetadata(null,
-        //        FrameworkPropertyMetadataOptions.AffectsRender |
-        //        FrameworkPropertyMetadataOptions.AffectsMeasure |
-        //        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
-        #region DepandencyProperty
-
-        public ObservableCollection<Thumb> MyThumbs
+        public ObservableCollection<KisoThumb> MyThumbs
         {
-            get { return (ObservableCollection<Thumb>)GetValue(MyThumbsProperty); }
+            get { return (ObservableCollection<KisoThumb>)GetValue(MyThumbsProperty); }
             set { SetValue(MyThumbsProperty, value); }
         }
         public static readonly DependencyProperty MyThumbsProperty =
-            DependencyProperty.Register(nameof(MyThumbs), typeof(ObservableCollection<Thumb>), typeof(GroupThumb), new PropertyMetadata(null));
-        #endregion
+            DependencyProperty.Register(nameof(MyThumbs), typeof(ObservableCollection<KisoThumb>), typeof(GroupThumb), new PropertyMetadata(null));
 
+
+        #endregion 依存関係プロパティ
+
+        #region コンストラクタ
 
         static GroupThumb()
         {
@@ -108,61 +156,286 @@ namespace _20241211
         }
         public GroupThumb()
         {
-            DataContext = this;
             MyThumbs = [];
             Loaded += GroupThumb_Loaded;
             MyThumbs.CollectionChanged += MyThumbs_CollectionChanged;
         }
 
-        //子要素を追加時に親要素のGroupThumbを追加、削除時には削除
-        private void MyThumbs_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.Action == NotifyCollectionChangedAction.Add)
-            {
-                if (e.NewItems?[0] is Thumb t)
-                {
-                    t.Tag = this;
-                }
-            }
-            else if (e.Action == NotifyCollectionChangedAction.Remove)
-            {
-                if (e.OldItems?[0] is Thumb t) { t.Tag = null; }
-            }
-        }
+        #endregion コンストラクタ
 
-        //TemplateのItemsControlのItemPanelのExCanvasを取得して、サイズをBinding
+        #region 初期化
+
         private void GroupThumb_Loaded(object sender, RoutedEventArgs e)
         {
-
-            if (GetTemplateChild("PART_ItemsControl") is ItemsControl ic)
+            var temp = GetTemplateChild("PART_ItemsControl");
+            if (temp is ItemsControl ic)
             {
-                var c = GetCanvas(ic);
-                if (c != null)
+                var ec = GetExCanvas(ic);
+                if (ec != null)
                 {
-                    _ = SetBinding(WidthProperty, new Binding() { Source = c, Path = new PropertyPath(ActualWidthProperty) });
-
-                    _ = SetBinding(HeightProperty, new Binding() { Source = c, Path = new PropertyPath(ActualHeightProperty) });
+                    _ = SetBinding(WidthProperty, new Binding() { Source = ec, Path = new PropertyPath(ActualWidthProperty) });
+                    _ = SetBinding(HeightProperty, new Binding() { Source = ec, Path = new PropertyPath(ActualHeightProperty) });
                 }
             }
         }
 
 
-        //子要素を辿ってCanvasを取り出す
-        private static Canvas? GetCanvas(DependencyObject d)
+        //子要素を辿ってExCanvasを取り出す
+        private static ExCanvas? GetExCanvas(DependencyObject d)
         {
-            if (d is Canvas canvas) return canvas;
+            if (d is ExCanvas canvas) return canvas;
             for (int i = 0; i < VisualTreeHelper.GetChildrenCount(d); i++)
             {
-                Canvas? c = GetCanvas(VisualTreeHelper.GetChild(d, i));
+                ExCanvas? c = GetExCanvas(VisualTreeHelper.GetChild(d, i));
                 if (c is not null) return c;
             }
             return null;
         }
 
 
+        private void MyThumbs_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems?[0] is KisoThumb nnt)
+            {
+                nnt.MyParentThumb = this;
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove && e.OldItems?[0] is KisoThumb ot)
+            {
+                ot.MyParentThumb = null;
+            }
+        }
+        #endregion 初期化
 
+        /// <summary>
+        /// 再配置、子要素の座標を元に位置を修正する
+        /// </summary>
+        public void ReLayout()
+        {
+            //すべての子要素で最も左上の座標を取得
+            double left = double.MaxValue; double top = double.MaxValue;
+            foreach (var item in MyThumbs)
+            {
+                if (left > item.MyLeft) { left = item.MyLeft; }
+                if (top > item.MyTop) { top = item.MyTop; }
+            }
+
+            //自身の座標と比較、同じ(変化なし)なら終了
+            if (left == MyLeft && top == MyTop) return;
+
+            //座標変化の場合は、自身と全ての子要素の座標を変更する
+            if (left != 0)
+            {
+                foreach (var item in MyThumbs) { item.MyLeft -= left; }
+                MyLeft += left;
+            }
+            if (top != 0)
+            {
+                foreach (var item in MyThumbs) { item.MyTop -= top; }
+                MyTop += top;
+            }
+
+            //ParentThumbにも伝播させる
+            MyParentThumb?.ReLayout();
+        }
+
+        /// <summary>
+        /// ReLayoutの左上座標取得をRectのUnion版、いまいちなので未使用
+        /// </summary>
+        public void ReLayout2()
+        {
+            //子要素すべてが収まる範囲Rectを計算
+            KisoThumb tt = MyThumbs[0];
+            Rect uRect = new(tt.MyLeft, tt.MyTop, tt.ActualWidth, tt.ActualHeight);
+            foreach (var item in MyThumbs)
+            {
+                Rect r = new(item.MyLeft, item.MyTop, item.ActualWidth, item.ActualHeight);
+                uRect.Union(r);
+            }
+
+            //今の範囲Rectと比較、座標変化なしなら終了
+            if (uRect.Left == MyLeft && uRect.Top == MyTop) return;
+
+            //座標変化の場合は、自身と全ての子要素の座標を変更する
+            if (uRect.Left != 0)
+            {
+                foreach (var item in MyThumbs) { item.MyLeft -= uRect.Left; }
+                MyLeft += uRect.Left;
+            }
+            if (uRect.Top != 0)
+            {
+                foreach (var item in MyThumbs) { item.MyTop -= uRect.Top; }
+                MyTop += uRect.Top;
+            }
+
+            //ParentThumbにも伝播させる
+            MyParentThumb?.ReLayout2();
+        }
 
     }
+
+
+    
+    //[ContentProperty(nameof(MyThumbs))]
+    //public class GroupThumb2 : KisoThumb
+    //{
+    //    #region 依存関係プロパティ
+
+    //    public ObservableCollection<KisoThumb> MyThumbs
+    //    {
+    //        get { return (ObservableCollection<KisoThumb>)GetValue(MyThumbsProperty); }
+    //        set { SetValue(MyThumbsProperty, value); }
+    //    }
+    //    public static readonly DependencyProperty MyThumbsProperty =
+    //        DependencyProperty.Register(nameof(MyThumbs), typeof(ObservableCollection<KisoThumb>), typeof(GroupThumb2), new PropertyMetadata(null));
+
+
+    //    //public ObservableCollection<KisoThumb> MyThumbs
+    //    //{
+    //    //    get { return (ObservableCollection<KisoThumb>)GetValue(MyThumbsProperty); }
+    //    //    set { SetValue(MyThumbsProperty, value); }
+    //    //}
+    //    //public static readonly DependencyProperty MyThumbsProperty =
+    //    //    DependencyProperty.Register(nameof(MyThumbs), typeof(ObservableCollection<KisoThumb>), typeof(GroupThumb2),
+    //    //        new FrameworkPropertyMetadata(null,
+    //    //            FrameworkPropertyMetadataOptions.AffectsRender |
+    //    //            FrameworkPropertyMetadataOptions.AffectsMeasure |
+    //    //            FrameworkPropertyMetadataOptions.AffectsArrange |
+    //    //            FrameworkPropertyMetadataOptions.Inherits |
+    //    //            FrameworkPropertyMetadataOptions.AffectsParentArrange |
+    //    //            FrameworkPropertyMetadataOptions.AffectsParentMeasure |
+    //    //            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    //    #endregion 依存関係プロパティ
+
+    //    #region コンストラクタ
+
+    //    static GroupThumb2()
+    //    {
+    //        DefaultStyleKeyProperty.OverrideMetadata(typeof(GroupThumb2), new FrameworkPropertyMetadata(typeof(GroupThumb2)));
+    //    }
+    //    public GroupThumb2()
+    //    {
+    //        MyThumbs = [];
+    //        Loaded += GroupThumb_Loaded;
+    //        MyThumbs.CollectionChanged += MyThumbs_CollectionChanged;
+            
+    //    }
+
+    //    #endregion コンストラクタ
+
+    //    #region 初期化
+
+    //    private void GroupThumb_Loaded(object sender, RoutedEventArgs e)
+    //    {
+    //        var temp = GetTemplateChild("PART_ItemsControl");
+    //        if (temp is ItemsControl ic)
+    //        {
+    //            var ec = GetCanvas(ic);
+    //            if (ec != null)
+    //            {
+    //                _ = SetBinding(WidthProperty, new Binding() { Source = ec, Path = new PropertyPath(ActualWidthProperty) });
+    //                _ = SetBinding(HeightProperty, new Binding() { Source = ec, Path = new PropertyPath(ActualHeightProperty) });
+    //            }
+    //        }
+            
+    //        ReLayout2();//これでXAMLで要素追加後にビルドするとサイズが入る、重要
+    //    }
+
+
+    //    //子要素を辿ってCanvasを取り出す
+    //    private static Canvas? GetCanvas(DependencyObject d)
+    //    {
+    //        if (d is Canvas canvas) return canvas;
+    //        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(d); i++)
+    //        {
+    //            Canvas? c = GetCanvas(VisualTreeHelper.GetChild(d, i));
+    //            if (c is not null) return c;
+    //        }
+    //        return null;
+    //    }
+
+
+    //    private void MyThumbs_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    //    {
+    //        if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems?[0] is KisoThumb nt)
+    //        {
+    //            nt.MyParentThumb2 = this;
+    //        }
+    //        else if (e.Action == NotifyCollectionChangedAction.Remove && e.OldItems?[0] is KisoThumb ot)
+    //        {
+    //            ot.MyParentThumb2 = null;
+    //        }
+    //    }
+    //    #endregion 初期化
+
+    //    ///// <summary>
+    //    ///// 再配置、子要素の座標を元に位置を修正する
+    //    ///// </summary>
+    //    //public void ReLayout()
+    //    //{
+    //    //    //すべての子要素で最も左上の座標を取得
+    //    //    double left = double.MaxValue; double top = double.MaxValue;
+    //    //    foreach (var item in MyThumbs)
+    //    //    {
+    //    //        if (left > item.MyLeft) { left = item.MyLeft; }
+    //    //        if (top > item.MyTop) { top = item.MyTop; }
+    //    //    }
+
+    //    //    //自身の座標と比較、同じ(変化なし)なら終了
+    //    //    if (left == MyLeft && top == MyTop) return;
+
+    //    //    //座標変化の場合は、自身と全ての子要素の座標を変更する
+    //    //    if (left != 0)
+    //    //    {
+    //    //        foreach (var item in MyThumbs) { item.MyLeft -= left; }
+    //    //        MyLeft += left;
+    //    //    }
+    //    //    if (top != 0)
+    //    //    {
+    //    //        foreach (var item in MyThumbs) { item.MyTop -= top; }
+    //    //        MyTop += top;
+    //    //    }
+
+    //    //    //ParentThumbにも伝播させる
+    //    //    MyParentThumb?.ReLayout();
+    //    //}
+
+    //    public void ReLayout2()
+    //    {
+    //        //すべての子要素で最も左上の座標を取得
+    //        double left = double.MaxValue; double top = double.MaxValue;
+    //        double right = double.MinValue; double bottom = double.MinValue;
+    //        foreach (var item in MyThumbs)
+    //        {
+    //            if (left > item.MyLeft) { left = item.MyLeft; }
+    //            if (top > item.MyTop) { top = item.MyTop; }
+    //            if (right < item.MyLeft + item.ActualWidth) { right = item.MyLeft + item.ActualWidth; }
+    //            if (bottom < item.MyTop + item.ActualHeight) { bottom = item.MyTop + item.ActualHeight; }
+    //        }
+
+    //        //サイズ修正
+    //        double w = right - left; double h = bottom - top;
+    //        if (w != ActualWidth) { Width = w; }
+    //        if (h != ActualHeight) { Height = h; }
+
+    //        //座標変化の場合は、自身と全ての子要素の座標を変更する
+    //        if (left != 0)
+    //        {
+    //            foreach (var item in MyThumbs) { item.MyLeft -= left; }
+    //            MyLeft += left;
+    //        }
+    //        if (top != 0)
+    //        {
+    //            foreach (var item in MyThumbs) { item.MyTop -= top; }
+    //            MyTop += top;
+    //        }
+
+    //        //ParentThumbにも伝播させる
+    //        MyParentThumb2?.ReLayout2();
+    //    }
+
+
+    //}
 
 
 
--- 20241211/MainWindow.xaml.cs
+++ 20241211/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 //スタイルの引き継ぎ
 //[WPF/xaml]BasedOnを使って元のstyleを受け継ぐ #C# - Qiita
@@ -24,5 +25,219 @@ namespace _20241211
         {
             InitializeComponent();
         }
+
+        private void KisoThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+
+            if (sender is KisoThumb t)
+            {
+                t.MyLeft += e.HorizontalChange;
+                t.MyTop += e.VerticalChange;
+                e.Handled = true;
+                GotFocus += MainWindow_GotFocus;
+                GotKeyboardFocus += MainWindow_GotKeyboardFocus;
+                PreviewGotKeyboardFocus += MainWindow_PreviewGotKeyboardFocus;
+                //GotFocus += (a, b) => { if (a is KisoThumb kiso) MyFocus.Text = "GotFocus = " + kiso.MyText; };
+                //GotKeyboardFocus += (a, b) => { if (sender is KisoThumb kiso) MyKeyFocus.Text = "GotKeyFocus = " + kiso.MyText; };
+                //PreviewGotKeyboardFocus += (a, b) => { if (sender is KisoThumb kiso) { MyPreKeyFocus.Text = "PreGotKeyFocus = " + kiso.MyText; } };
+            }
+
+
+
+        }
+
+        private void MainWindow_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (e.NewFocus is KisoThumb kiso) MyKeyFocus.Text = "GotKeyFocus = " + kiso.MyText;
+
+        }
+
+        private void MainWindow_PreviewGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (e.NewFocus is KisoThumb kiso) MyPreKeyFocus.Text = "PreGotKeyFocus = " + kiso.MyText;
+        }
+
+        private void MainWindow_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is KisoThumb kiso) MyFocus.Text = "GotFocus = " + kiso.MyText;
+        }
+
+        private void KisoThumb_DragDeltaSleep1(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is KisoThumb t)
+            {
+                if (t.MyLeft + t.ActualWidth > MyScroll.ActualWidth)
+                {
+                    MyScroll.ScrollToHorizontalOffset(MyScroll.HorizontalOffset + e.HorizontalChange);
+                    t.MyLeft += e.HorizontalChange;
+                    Thread.Sleep(100);
+                }
+                else
+                {
+                    t.MyLeft += e.HorizontalChange;
+                }
+
+                if (t.MyTop + t.ActualHeight > MyScroll.ActualHeight)
+                {
+                    MyScroll.ScrollToVerticalOffset(MyScroll.VerticalOffset + e.VerticalChange);
+                    t.MyTop += e.VerticalChange;
+                    Thread.Sleep(100);
+                }
+                else
+                {
+                    t.MyTop += e.VerticalChange;
+                }
+                e.Handled = true;
+            }
+
+        }
+        private void KisoThumb_DragDeltaSleep11(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is KisoThumb t)
+            {
+                if (t.MyLeft + t.ActualWidth > MyScroll.ActualWidth)
+                {
+                    MyScroll.ScrollToHorizontalOffset(MyScroll.HorizontalOffset + e.HorizontalChange);
+                    t.MyLeft += e.HorizontalChange;
+                    Thread.Sleep(100);
+                }
+                else
+                {
+                    t.MyLeft += e.HorizontalChange;
+                }
+
+                if (t.MyTop + t.ActualHeight > MyScroll.ActualHeight)
+                {
+                    MyScroll.ScrollToVerticalOffset(MyScroll.VerticalOffset + e.VerticalChange);
+                    t.MyTop += e.VerticalChange;
+                    Thread.Sleep(100);
+                }
+                else
+                {
+                    t.MyTop += e.VerticalChange;
+                }
+                e.Handled = true;
+            }
+
+        }
+        private void KisoThumb_DragDeltaSleep2(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is KisoThumb t)
+            {
+                if (t.MyLeft + t.ActualWidth > MyScroll.ActualWidth)
+                {
+                    MyScroll.ScrollToHorizontalOffset(MyScroll.HorizontalOffset + e.HorizontalChange);
+                    t.MyLeft += e.HorizontalChange;
+                    Thread.Sleep(100);
+                }
+                else
+                {
+                    t.MyLeft += e.HorizontalChange;
+                }
+
+                if (t.MyTop + t.ActualHeight > MyScroll.ActualHeight)
+                {
+                    MyScroll.ScrollToVerticalOffset(MyScroll.VerticalOffset + e.VerticalChange);
+                    t.MyTop += e.VerticalChange;
+                    Thread.Sleep(100);
+                }
+                else
+                {
+                    t.MyTop += e.VerticalChange;
+                }
+
+
+
+                if (t.MyLeft < 0 || t.MyTop < 0)
+                {
+                    Thread.Sleep(100);
+                    t.MyParentThumb?.ReLayout();
+                    MyRootGroup.MyLeft = 0;
+                    MyRootGroup.MyTop = 0;
+                }
+                //else
+                //{
+                //    if (MyScroll.VerticalScrollBarVisibility == ScrollBarVisibility.Visible)
+                //    {
+                //        MyScroll.ScrollToVerticalOffset(MyScroll.VerticalOffset + e.VerticalChange);
+                //    t.MyParentThumb?.ReLayout();
+                //    }
+                //}
+
+                t.MyParentThumb?.ReLayout();
+                e.Handled = true;
+            }
+
+        }
+
+
+        private void KisoThumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            if (sender is KisoThumb t)
+            {
+                t.MyParentThumb?.ReLayout();
+
+                MyRootGroup.MyLeft = 0;
+                MyRootGroup.MyTop = 0;
+                //t.Focus();
+
+                e.Handled = true;
+            }
+        }
+
+        private void KisoThumb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is KisoThumb t)
+            {
+                if (e.Key == Key.Left)
+                {
+                    t.MyLeft -= 10;
+                    var hor = MyScroll.HorizontalOffset;
+                    var scw = MyScroll.ActualWidth;
+                    var rig = t.MyLeft + t.ActualWidth;
+                    if (t.MyLeft < 0)
+                    {
+                        t.MyParentThumb?.ReLayout();
+                    }
+                    e.Handled = true;
+                }
+                else if (e.Key == Key.Right)
+                {
+                    t.MyLeft += 10;
+                    //Thumbの右端が見切れたら、その分をスクロールさせる
+                    var thumbRight = t.MyLeft + t.ActualWidth;
+                    var scrollRight = MyScroll.ActualWidth + MyScroll.HorizontalOffset;
+                    if (thumbRight > scrollRight)
+                    {
+                        MyScroll.ScrollToHorizontalOffset(MyScroll.HorizontalOffset + (thumbRight - scrollRight));
+                    }
+                    e.Handled = true;
+                }
+            }
+        }
+
+
+        //private void KisoThumb_DragDelta2(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        //{
+        //    if (sender is KisoThumb t)
+        //    {
+        //        t.MyLeft += e.HorizontalChange;
+        //        t.MyTop += e.VerticalChange;
+        //        e.Handled = true;
+        //    }
+        //}
+
+        //private void KisoThumb_DragCompleted2(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        //{
+        //    if (sender is KisoThumb t)
+        //    {
+        //        t.MyParentThumb2?.ReLayout2();
+
+        //        MyRootGroup.MyLeft = 0;
+        //        MyRootGroup.MyTop = 0;
+
+        //        e.Handled = true;
+        //    }
+        //}
     }
 }
