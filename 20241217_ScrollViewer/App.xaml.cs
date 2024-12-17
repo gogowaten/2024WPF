74bf6a39223e6f60e3454d582c09bd827bb0b91b
waten <gogowaten@yahoo.co.jp>
2024-12-17 12:59:38 +0900

    202412171259

---
+++ 20241217_ScrollViewer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20241217_ScrollViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
