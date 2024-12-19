373e2133fd53f7f76a47cdcaff55522d47f6e195
waten <gogowaten@yahoo.co.jp>
2024-12-19 13:34:42 +0900

    202412191334

---
+++ 20241219_拡大表示時のマウスドラッグ移動/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20241219_拡大表示時のマウスドラッグ移動
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
--- 20241219_拡大表示時のマウスドラッグ移動/MainWindow.xaml.cs
+++ 20241219_拡大表示時のマウスドラッグ移動/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _20241218
+namespace _20241219_拡大表示時のマウスドラッグ移動
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -30,7 +22,6 @@ namespace _20241218
         {
             InitializeComponent();
             DataContext = this;
-            MyComboScaleMode.ItemsSource = Enum.GetValues(typeof(BitmapScalingMode));
         }
 
         private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
@@ -51,50 +42,24 @@ namespace _20241218
 
                 if (hc > 0)
                 {
-                    Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
+                    Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5));
                 }
                 else if (hc < 0)
                 {
-                    Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc - 0.5));
+                    Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5));
                 }
 
                 double vc = e.VerticalChange;
                 if (vc > 0)
                 {
-                    Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc + 0.5));
+                    Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5));
                 }
                 else if (vc < 0)
                 {
-                    Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc - 0.5));
+                    Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5));
                 }
                 e.Handled = true;
             }
-            //if (sender is Thumb t)
-            //{
-            //    double hc = e.HorizontalChange;
-
-            //    if (hc > 0)
-            //    {
-            //        //Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
-            //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5));
-            //    }
-            //    else if (hc < 0)
-            //    {
-            //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5));
-            //    }
-
-            //    double vc = e.VerticalChange;
-            //    if (vc > 0)
-            //    {
-            //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5));
-            //    }
-            //    else if (vc < 0)
-            //    {
-            //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5));
-            //    }
-            //    e.Handled = true;
-            //}
-
             //if (sender is Thumb t)
             //{
             //    double hc = e.HorizontalChange;
@@ -144,24 +109,6 @@ namespace _20241218
         private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
         {
 
-        }
-
-        private void Button_Click_Rounding(object sender, RoutedEventArgs e)
-        {
-            MyCanvas.UseLayoutRounding = !MyCanvas.UseLayoutRounding;
-        }
-
-
-        private void Button_Click_EdgeMode(object sender, RoutedEventArgs e)
-        {
-            if (RenderOptions.GetEdgeMode(MyCanvas) == EdgeMode.Unspecified)
-            {
-                RenderOptions.SetEdgeMode(MyCanvas, EdgeMode.Aliased);
-            }
-            else
-            {
-                RenderOptions.SetEdgeMode(MyCanvas, EdgeMode.Unspecified);
-            }
         }
     }
 }
