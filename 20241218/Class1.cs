60b95808299573ce8e8c761056a39e768f36f53c
waten <gogowaten@yahoo.co.jp>
2024-12-20 21:03:10 +0900

    202412202103

---
+++ 20241218/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace _20241218
+{
+
+
+
+
+    public class FocusAdorner : Adorner
+    {
+        // Be sure to call the base class constructor.
+        public FocusAdorner(UIElement adornedElement) : base(adornedElement)
+        {
+            IsHitTestVisible = false;
+        }
+
+        // A common way to implement an adorner's rendering behavior is to override the OnRender
+        // method, which is called by the layout system as part of a rendering pass.
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var drawRect = LayoutInformation.GetLayoutSlot((FrameworkElement)this.AdornedElement);
+            drawRect = new Rect(1, 1, drawRect.Width - 2, drawRect.Height - 2);
+
+
+            // Some arbitrary drawing implements.
+            //SolidColorBrush renderBrush = new(Colors.Transparent);
+            Pen renderPen = new(new SolidColorBrush(Colors.Black), 2)
+            {
+                DashStyle = new DashStyle([2, 3], 0)
+            };
+
+            drawingContext.DrawRoundedRectangle(Brushes.Transparent, renderPen, drawRect, 3, 3);
+        }
+    }
+
+
+
+}
--- 20241218/MainWindow.xaml.cs
+++ 20241218/MainWindow.xaml.cs
@@ -17,151 +17,132 @@ namespace _20241218
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        public int MyScale
-        {
-            get { return (int)GetValue(MyScaleProperty); }
-            set { SetValue(MyScaleProperty, value); }
-        }
-        public static readonly DependencyProperty MyScaleProperty =
-            DependencyProperty.Register(nameof(MyScale), typeof(int), typeof(MainWindow), new PropertyMetadata(1));
+       
 
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
-            MyComboScaleMode.ItemsSource = Enum.GetValues(typeof(BitmapScalingMode));
+            
         }
 
-        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        private void TextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (sender is Thumb t)
+            var send = (StackPanel)sender;
+            var layer = AdornerLayer.GetAdornerLayer(send);
+            foreach (var item in layer.GetAdorners(send))
             {
-                Canvas.SetLeft(t, Canvas.GetLeft(t) + e.HorizontalChange);
-                Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
-                e.Handled = true;
+                layer.Remove(item);
             }
         }
 
-        private void Thumb_DragDelta2(object sender, DragDeltaEventArgs e)
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (sender is Thumb t)
-            {
-                double hc = e.HorizontalChange;
-
-                if (hc > 0)
-                {
-                    Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
-                }
-                else if (hc < 0)
-                {
-                    Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc - 0.5));
-                }
-
-                double vc = e.VerticalChange;
-                if (vc > 0)
-                {
-                    Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc + 0.5));
-                }
-                else if (vc < 0)
-                {
-                    Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc - 0.5));
-                }
-                e.Handled = true;
-            }
-            //if (sender is Thumb t)
-            //{
-            //    double hc = e.HorizontalChange;
-
-            //    if (hc > 0)
-            //    {
-            //        //Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
-            //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5));
-            //    }
-            //    else if (hc < 0)
-            //    {
-            //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5));
-            //    }
-
-            //    double vc = e.VerticalChange;
-            //    if (vc > 0)
-            //    {
-            //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5));
-            //    }
-            //    else if (vc < 0)
-            //    {
-            //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5));
-            //    }
-            //    e.Handled = true;
-            //}
-
-            //if (sender is Thumb t)
-            //{
-            //    double hc = e.HorizontalChange;
-
-            //    if (hc > 0)
-            //    {
-            //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5) * MyScale);
-            //    }
-            //    else if (hc < 0)
-            //    {
-            //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5) * MyScale);
-            //    }
-
-            //    double vc = e.VerticalChange;
-            //    if (vc > 0)
-            //    {
-            //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5) * MyScale);
-            //    }
-            //    else if (vc < 0)
-            //    {
-            //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5) * MyScale);
-            //    }
-            //    e.Handled = true;
-            //}
+            var send = (StackPanel)sender;
+            AdornerLayer.GetAdornerLayer(send).Add(new FocusAdorner(send));
         }
 
-        private void Button_Click_100(object sender, RoutedEventArgs e)
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            MyScale = 1;
-        }
-
-        private void Button_Click_200(object sender, RoutedEventArgs e)
-        {
-            MyScale = 2;
-        }
-
-        private void Button_Click_1000(object sender, RoutedEventArgs e)
-        {
-            MyScale = 10;
-        }
-
-        private void Button_Click_300(object sender, RoutedEventArgs e)
-        {
-            MyScale = 3;
-        }
-
-        private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_Rounding(object sender, RoutedEventArgs e)
-        {
-            MyCanvas.UseLayoutRounding = !MyCanvas.UseLayoutRounding;
+            var send = (StackPanel)sender;
+            Keyboard.Focus(send);
         }
 
 
-        private void Button_Click_EdgeMode(object sender, RoutedEventArgs e)
-        {
-            if (RenderOptions.GetEdgeMode(MyCanvas) == EdgeMode.Unspecified)
-            {
-                RenderOptions.SetEdgeMode(MyCanvas, EdgeMode.Aliased);
-            }
-            else
-            {
-                RenderOptions.SetEdgeMode(MyCanvas, EdgeMode.Unspecified);
-            }
-        }
+
+
+
+
+
+
+        //private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        //{
+        //    if (sender is Thumb t)
+        //    {
+        //        Canvas.SetLeft(t, Canvas.GetLeft(t) + e.HorizontalChange);
+        //        Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
+        //        e.Handled = true;
+        //    }
+        //}
+
+        //private void Thumb_DragDelta2(object sender, DragDeltaEventArgs e)
+        //{
+        //    if (sender is Thumb t)
+        //    {
+        //        double hc = e.HorizontalChange;
+
+        //        if (hc > 0)
+        //        {
+        //            Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
+        //        }
+        //        else if (hc < 0)
+        //        {
+        //            Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc - 0.5));
+        //        }
+
+        //        double vc = e.VerticalChange;
+        //        if (vc > 0)
+        //        {
+        //            Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc + 0.5));
+        //        }
+        //        else if (vc < 0)
+        //        {
+        //            Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc - 0.5));
+        //        }
+        //        e.Handled = true;
+        //    }
+        //    //if (sender is Thumb t)
+        //    //{
+        //    //    double hc = e.HorizontalChange;
+
+        //    //    if (hc > 0)
+        //    //    {
+        //    //        //Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
+        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5));
+        //    //    }
+        //    //    else if (hc < 0)
+        //    //    {
+        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5));
+        //    //    }
+
+        //    //    double vc = e.VerticalChange;
+        //    //    if (vc > 0)
+        //    //    {
+        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5));
+        //    //    }
+        //    //    else if (vc < 0)
+        //    //    {
+        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5));
+        //    //    }
+        //    //    e.Handled = true;
+        //    //}
+
+        //    //if (sender is Thumb t)
+        //    //{
+        //    //    double hc = e.HorizontalChange;
+
+        //    //    if (hc > 0)
+        //    //    {
+        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5) * MyScale);
+        //    //    }
+        //    //    else if (hc < 0)
+        //    //    {
+        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5) * MyScale);
+        //    //    }
+
+        //    //    double vc = e.VerticalChange;
+        //    //    if (vc > 0)
+        //    //    {
+        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5) * MyScale);
+        //    //    }
+        //    //    else if (vc < 0)
+        //    //    {
+        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5) * MyScale);
+        //    //    }
+        //    //    e.Handled = true;
+        //    //}
+        //}
+
+
+
     }
 }
