ce8377219e8b935a464eedef3e3eac9db4b343ca
waten <gogowaten@yahoo.co.jp>
2024-12-21 18:49:23 +0900

    202412211849

--- 20241218/MainWindow.xaml.cs
+++ 20241218/MainWindow.xaml.cs
@@ -17,131 +17,97 @@ namespace _20241218
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+
 
         public MainWindow()
         {
             InitializeComponent();
-            
+
+
         }
 
-        private void TextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            var send = (StackPanel)sender;
-            var layer = AdornerLayer.GetAdornerLayer(send);
-            foreach (var item in layer.GetAdorners(send))
+            if (sender is KisoThumb t)
             {
-                layer.Remove(item);
+                t.MyLeft += e.HorizontalChange;
+                t.MyTop += e.VerticalChange;
+                e.Handled = true;
             }
         }
 
-        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            var send = (StackPanel)sender;
-            AdornerLayer.GetAdornerLayer(send).Add(new FocusAdorner(send));
-        }
-
-        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            var send = (StackPanel)sender;
-            Keyboard.Focus(send);
-        }
-
-
-
-
-
-
-
-
-        //private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
-        //{
-        //    if (sender is Thumb t)
-        //    {
-        //        Canvas.SetLeft(t, Canvas.GetLeft(t) + e.HorizontalChange);
-        //        Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
-        //        e.Handled = true;
-        //    }
-        //}
-
+        ////失敗
+        ////スクロールオフセットでスクロール位置を固定はできない
         //private void Thumb_DragDelta2(object sender, DragDeltaEventArgs e)
         //{
-        //    if (sender is Thumb t)
+        //    if (sender is KisoThumb t)
         //    {
-        //        double hc = e.HorizontalChange;
+        //        var scrollWidth = MyScrollV.ActualWidth;
+        //        var scrollHOffset = MyScrollV.HorizontalOffset;
+        //        var scrollRight = MyScrollV.ActualWidth + MyScrollV.HorizontalOffset;
+        //        double parentLeft = t.MyParentThumb?.MyLeft ?? 0;
+        //        var thumbRight = parentLeft + t.MyLeft + t.ActualWidth;
 
-        //        if (hc > 0)
+        //        t.MyLeft += e.HorizontalChange;
+        //        t.MyTop += e.VerticalChange;
+
+
+        //        var thumbRight2 = parentLeft + t.MyLeft + t.ActualWidth;
+        //        //固定
+        //        if (scrollRight > thumbRight2)
         //        {
-        //            Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
-        //        }
-        //        else if (hc < 0)
-        //        {
-        //            Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc - 0.5));
+        //            MyScrollV.ScrollToHorizontalOffset(scrollHOffset);
         //        }
 
-        //        double vc = e.VerticalChange;
-        //        if (vc > 0)
-        //        {
-        //            Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc + 0.5));
-        //        }
-        //        else if (vc < 0)
-        //        {
-        //            Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc - 0.5));
-        //        }
         //        e.Handled = true;
         //    }
-        //    //if (sender is Thumb t)
-        //    //{
-        //    //    double hc = e.HorizontalChange;
-
-        //    //    if (hc > 0)
-        //    //    {
-        //    //        //Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc + 0.5));
-        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5));
-        //    //    }
-        //    //    else if (hc < 0)
-        //    //    {
-        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5));
-        //    //    }
-
-        //    //    double vc = e.VerticalChange;
-        //    //    if (vc > 0)
-        //    //    {
-        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5));
-        //    //    }
-        //    //    else if (vc < 0)
-        //    //    {
-        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5));
-        //    //    }
-        //    //    e.Handled = true;
-        //    //}
-
-        //    //if (sender is Thumb t)
-        //    //{
-        //    //    double hc = e.HorizontalChange;
-
-        //    //    if (hc > 0)
-        //    //    {
-        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale + 0.5) * MyScale);
-        //    //    }
-        //    //    else if (hc < 0)
-        //    //    {
-        //    //        Canvas.SetLeft(t, Canvas.GetLeft(t) + (int)(hc / MyScale - 0.5) * MyScale);
-        //    //    }
-
-        //    //    double vc = e.VerticalChange;
-        //    //    if (vc > 0)
-        //    //    {
-        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale + 0.5) * MyScale);
-        //    //    }
-        //    //    else if (vc < 0)
-        //    //    {
-        //    //        Canvas.SetTop(t, Canvas.GetTop(t) + (int)(vc / MyScale - 0.5) * MyScale);
-        //    //    }
-        //    //    e.Handled = true;
-        //    //}
         //}
 
+
+        /// <summary>
+        /// ドラッグ移動終了時
+        /// アンカーThumbをCollapsed化と再配置後に親要素の再配置
+        /// </summary>
+        private void KisoThumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            if (sender is KisoThumb t && t.MyParentThumb is not null)
+            {
+                if (t.MyParentThumb is GroupThumb gt)
+                {
+                    AnchorThumb anchor = gt.MyAnchorThumb;
+                    anchor.Visibility = Visibility.Collapsed;
+                    anchor.MyLeft = t.MyLeft;
+                    anchor.MyTop = t.MyTop;
+                }
+                t.MyParentThumb.ReLayout3();
+            }
+
+            //イベントをここで停止
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// ドラッグ移動開始時
+        /// アンカーThumbをHidden化、サイズと位置を移動要素に合わせる
+        /// </summary>
+        private void KisoThumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            if (e.Source is KisoThumb t)
+            {
+                //アンカーThumbをHidden、在るけど見えないだけ
+                if (t.MyParentThumb is GroupThumb gt)
+                {
+                    AnchorThumb anchor = gt.MyAnchorThumb;
+                    anchor.Visibility = Visibility.Hidden;
+                    anchor.Width = t.ActualWidth;
+                    anchor.Height = t.ActualHeight;
+                    anchor.MyLeft = t.MyLeft;
+                    anchor.MyTop = t.MyTop;
+
+                }
+            }
+        }
 
 
     }
