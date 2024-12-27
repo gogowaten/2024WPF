db2eef5873febdfda38bcf30a8eb6722c5324608
waten <gogowaten@yahoo.co.jp>
2024-12-27 13:28:55 +0900

    202412271328

---
+++ 20241227_StyleSelector/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace _20241227_StyleSelector
+{
+    public enum DataType { None = 0, Text, Ellipse, }
+    public class MyData : DependencyObject
+    {
+
+        public MyData(DataType type, string text)
+        {
+            MyText = text;
+            switch (type)
+            {
+                case DataType.None:
+                    Type = DataType.None; break;
+                case DataType.Text:
+                    Type = DataType.Text; break;
+                case DataType.Ellipse:
+                    Type = DataType.Ellipse; break;
+                default:
+                    break;
+            }
+        }
+
+        public DataType Type { get; }
+
+        public string MyText
+        {
+            get { return (string)GetValue(MyTextProperty); }
+            set { SetValue(MyTextProperty, value); }
+        }
+        public static readonly DependencyProperty MyTextProperty =
+            DependencyProperty.Register(nameof(MyText), typeof(string), typeof(MyData), new PropertyMetadata(string.Empty));
+
+
+        public double MyLeft
+        {
+            get { return (double)GetValue(MyLeftProperty); }
+            set { SetValue(MyLeftProperty, value); }
+        }
+        public static readonly DependencyProperty MyLeftProperty =
+            DependencyProperty.Register(nameof(MyLeft), typeof(double), typeof(MyData), new PropertyMetadata(10.0));
+
+        public double MyTop
+        {
+            get { return (double)GetValue(MyTopProperty); }
+            set { SetValue(MyTopProperty, value); }
+        }
+        public static readonly DependencyProperty MyTopProperty =
+            DependencyProperty.Register(nameof(MyTop), typeof(double), typeof(MyData), new PropertyMetadata(0.0));
+
+    }
+
+    public class MySelector : StyleSelector
+    {
+        public Style? Style1 { get; set; }
+        public Style? Style2 { get; set; }
+
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+
+            if (item is not MyData data)
+            {
+                return base.SelectStyle(item, container);
+            }
+            if (Style1 != null && Style2 != null)
+            {
+
+                return data.Type switch
+                {
+                    DataType.Text => Style1,
+                    DataType.Ellipse => Style2,
+                    _ => base.SelectStyle(item, container)
+                };
+            }
+            return base.SelectStyle(item, container);
+        }
+    }
+
+}
--- 20241227_StyleSelector/MainWindow.xaml.cs
+++ 20241227_StyleSelector/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -9,6 +10,20 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+//StyleSelector使ってみた
+//DataをBindingして表示をDataの種類によって変更するのはできるけど、
+//ListBoxをコンテナにした場合はドラッグ移動ができない、もしくはできたとしても難しそう
+//ListBoxに追加できるのはListBoxItem型だけなのが良くない
+//試しにListBoxItemのTemplateをThumbにしても、それはListBoxItemには変わりないので
+//ThumbのDragDeltaイベントは使えない
+//また、逆にThumbのTemplateをListBoxItemにしたら、それはThumbなので
+//ListBoxには追加できないはず(試してない)
+//ってことで無理、たぶんTreeViewでも同じだと思う、TreeViewに追加できるのはTreeViewItemだけ
+
+//なので、StyleSelectorの使い道としては、複雑な動きの制御はしないで
+//表示関連に絞れば良さそう、動きではクリックしたときに少し表示を変化させるとかはできそう
+
 namespace _20241227_StyleSelector
 {
     /// <summary>
@@ -16,9 +31,51 @@ namespace _20241227_StyleSelector
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<MyData> MyDatas { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+
+            MyDatas = [];
+            MyDatas.Add(new(DataType.Text, "text111111111") { MyLeft = 30, MyTop = 30 });
+            MyDatas.Add(new(DataType.Ellipse, "text22222222") { MyLeft = 20, MyTop = 60 });
+            DataContext = MyDatas;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var test = this.Resources["testkey"];
+            //var neko = this.Resources;
+
+            //var inu = this.FindResource("testkey");
+            //var uma = MyGrid.Resources["MyStyle1"];//ok
+            //var uma = MyDockPanel.Resources["MyStyle1"];//null
+            //var uma = this.Resources["MyStyle1"];//null
+            //var uma = this.FindResource("MyStyle1");//error
+            //ItemsControl tako = ItemsControl.ItemsControlFromItemContainer(this);
+        }
+
+        private void Thumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            if (sender is Thumb t)
+            {
+                var pare = VisualTreeHelper.GetParent(t);
+                var sou = e.Source.GetType();
+                var ori = e.OriginalSource.GetType;
+                var hori = Canvas.GetLeft(t);
+                Canvas.SetLeft(t, Canvas.GetLeft(t) + e.HorizontalChange);
+                Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyDatas[0].MyLeft += 10;
+            MyDatas[1].MyTop += 10;
         }
     }
+
+
 }
