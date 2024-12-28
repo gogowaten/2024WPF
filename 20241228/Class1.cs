d4194a9c3c45d4fd62725f5f28ae11433130a0e2
waten <gogowaten@yahoo.co.jp>
2024-12-28 18:21:49 +0900

    202412281821

---
+++ 20241228/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _20241228
+{
+    /// <summary>
+    /// データタイプの識別用列挙体
+    /// </summary>
+    public enum ThumbType { None = 0, Text, Ellipse, Rect, Group }
+
+    /// <summary>
+    /// データ用
+    /// 殆どを依存関係プロパティにしているのは、値を更新したときにBinding先に通知するため
+    /// </summary>
+    public class MyData : DependencyObject
+    {
+        public ThumbType Type { get; }
+
+        public MyData(ThumbType type)
+        {
+            this.Type = type;
+            MyDatas = [];
+        }
+
+        #region 依存関係プロパティ
+
+        public string MyText
+        {
+            get { return (string)GetValue(MyTextProperty); }
+            set { SetValue(MyTextProperty, value); }
+        }
+        public static readonly DependencyProperty MyTextProperty =
+            DependencyProperty.Register(nameof(MyText), typeof(string), typeof(MyData), new PropertyMetadata(string.Empty));
+
+
+        public double MyLeft
+        {
+            get { return (double)GetValue(MyLeftProperty); }
+            set { SetValue(MyLeftProperty, value); }
+        }
+        public static readonly DependencyProperty MyLeftProperty =
+            DependencyProperty.Register(nameof(MyLeft), typeof(double), typeof(MyData), new PropertyMetadata(0.0));
+
+        public double MyTop
+        {
+            get { return (double)GetValue(MyTopProperty); }
+            set { SetValue(MyTopProperty, value); }
+        }
+        public static readonly DependencyProperty MyTopProperty =
+            DependencyProperty.Register(nameof(MyTop), typeof(double), typeof(MyData), new PropertyMetadata(0.0));
+
+        public double MyVolume
+        {
+            get { return (double)GetValue(MyVolumeProperty); }
+            set { SetValue(MyVolumeProperty, value); }
+        }
+        public static readonly DependencyProperty MyVolumeProperty =
+            DependencyProperty.Register(nameof(MyVolume), typeof(double), typeof(MyData), new PropertyMetadata(30.0));
+
+
+        public Brush MyBrush
+        {
+            get { return (Brush)GetValue(MyBrushProperty); }
+            set { SetValue(MyBrushProperty, value); }
+        }
+        public static readonly DependencyProperty MyBrushProperty =
+            DependencyProperty.Register(nameof(MyBrush), typeof(Brush), typeof(MyData), new PropertyMetadata(Brushes.Transparent));
+
+
+        public ObservableCollection<MyData> MyDatas
+        {
+            get { return (ObservableCollection<MyData>)GetValue(MyDatasProperty); }
+            set { SetValue(MyDatasProperty, value); }
+        }
+        public static readonly DependencyProperty MyDatasProperty =
+            DependencyProperty.Register(nameof(MyDatas), typeof(ObservableCollection<MyData>), typeof(MyData), new PropertyMetadata(null));
+
+        #endregion 依存関係プロパティ
+    }
+
+
+    public class MyDTSelector : DataTemplateSelector
+    {
+        public DataTemplate? DT1 { get; set; }
+        public DataTemplate? DT2 { get; set; }
+        public DataTemplate? DT3 { get; set; }
+        public DataTemplate? DT4 { get; set; }
+
+        /// <summary>
+        /// 今回の場合だと、引数のitemにMyDataが入っているので、
+        /// データタイプを識別してそれぞれに合ったDataTemplateを返している
+        /// それぞれのDataTemplateの設定はXAMLの方で行っている
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        public override DataTemplate? SelectTemplate(object item, DependencyObject container)
+        {
+
+            if (item is not MyData)
+            {
+                return base.SelectTemplate(item, container);
+            }
+            else if (item is MyData dd)
+            {
+                if (dd.Type == ThumbType.Text) { return DT1; }
+                else if (dd.Type == ThumbType.Ellipse) { return DT2; }
+                else if (dd.Type == ThumbType.Rect) { return DT3; }
+                else if (dd.Type == ThumbType.Group) { return DT4; }
+                else { return base.SelectTemplate(item, container); }
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+
+}
---
+++ 20241228/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20241228
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<MyData> MyDatas { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyDatas = [
+                new MyData(ThumbType.Text)
+                {
+                    MyLeft = 30,
+                    MyTop = 10,
+                    MyText = "textblock thumb"
+                },
+                new MyData(ThumbType.Ellipse)
+                {
+                    MyLeft = 30,
+                    MyTop = 80,
+                    MyVolume = 100,
+                    MyBrush = Brushes.Gold
+                },
+                new MyData(ThumbType.Rect)
+                {
+                    MyLeft = 130,
+                    MyTop= 140,
+                    MyVolume = 100,
+                    MyBrush = Brushes.DodgerBlue
+                },
+                new MyData(ThumbType.Group)
+                {
+                    MyLeft= 30,
+                    MyTop= 30,
+
+                }];
+
+            DataContext = MyDatas;
+            MyData data = new(ThumbType.Text) { MyLeft = 30, MyTop = 10, MyText = "group item 1" };
+            MyDatas[3].MyDatas.Add(data);
+            MyDatas[3].MyDatas.Add(new(ThumbType.Ellipse) { MyLeft = 30, MyTop = 80, MyVolume = 30, MyBrush = Brushes.Salmon });
+
+        }
+
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is Thumb t)
+            {
+                Canvas.SetLeft(t, Canvas.GetLeft(t) + e.HorizontalChange);
+                Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
+                e.Handled = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyDatas[0].MyText += "👍️";
+            MyDatas[1].MyLeft += 10;
+            MyDatas[2].MyTop += 10;
+        }
+
+    }
+}
