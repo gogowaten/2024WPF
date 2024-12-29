76b48e7b67e6f6aba54bc526ed2798234c85d913
waten <gogowaten@yahoo.co.jp>
2024-12-29 22:59:29 +0900

    202412292259

--- 20241228/MainWindow.xaml.cs
+++ 20241228/MainWindow.xaml.cs
@@ -94,5 +94,51 @@ namespace _20241228
                 e.Handled = true;
             }
         }
+
+
+        /// <summary>
+        /// ドラッグ移動開始時
+        /// アンカーThumbをHidden化、サイズと位置を移動要素に合わせる
+        /// </summary>
+        private void KisoThumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            if (e.Source is KisoThumb t)
+            {
+                //アンカーThumbをHidden、在るけど見えないだけ
+                if (t.MyParentThumb is GroupThumb gt)
+                {
+                    AnchorThumb anchor = gt.MyAnchorThumb;
+                    anchor.Visibility = Visibility.Hidden;
+                    anchor.Width = t.ActualWidth;
+                    anchor.Height = t.ActualHeight;
+                    anchor.MyLeft = t.MyLeft;
+                    anchor.MyTop = t.MyTop;
+
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// ドラッグ移動終了時
+        /// アンカーThumbをCollapsed化と再配置後に親要素の再配置
+        /// </summary>
+        private void KisoThumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            if (sender is KisoThumb t && t.MyParentThumb is not null)
+            {
+                if (t.MyParentThumb is GroupThumb gt)
+                {
+                    AnchorThumb anchor = gt.MyAnchorThumb;
+                    anchor.Visibility = Visibility.Collapsed;
+                    anchor.MyLeft = t.MyLeft;
+                    anchor.MyTop = t.MyTop;
+                }
+                t.MyParentThumb.ReLayout3();
+            }
+
+            //イベントをここで停止
+            e.Handled = true;
+        }
     }
 }
